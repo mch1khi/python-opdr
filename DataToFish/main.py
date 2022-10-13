8c9df5852201a484d62c6a681fce3658ae7f0430
mchii <200535@student.glu.nl>
2022-10-13 14:51:19 +0200

    data to fish af

---
+++ DataToFish/main.py
@@ -0,0 +1,42 @@
+import pyautogui
+import time
+# hierboven import ik een ai die dn wat met de code beneden doet
+
+# hier links zoekt hij de eerste de beste bestand op
+pyautogui.click(65, 55, clicks=2)
+pyautogui.hotkey('delete')
+# hier haalt hij de text weg
+time.sleep(2)
+# hier typet inplaats van de naam een andere text
+pyautogui.write('jij bient gehakt')
+pyautogui.hotkey('enter')
+
+time.sleep(1)
+# hier links zoekt hij de eerste de beste bestand op
+pyautogui.click(65, 55, clicks=2)
+pyautogui.hotkey('delete')
+# hier haalt hij de text weg
+time.sleep(2)
+# hier typet inplaats van de naam een andere text
+pyautogui.write('grapjuh')
+pyautogui.hotkey('enter')
+
+time.sleep(1)
+# hier links zoekt hij de eerste de beste bestand op
+pyautogui.click(65, 55, clicks=2)
+pyautogui.hotkey('delete')
+# hier haalt hij de text weg
+time.sleep(2)
+# hier typet inplaats van de naam een andere text
+pyautogui.write('ik verander de naam wel terug')
+pyautogui.hotkey('enter')
+
+time.sleep(1)
+# hier links zoekt hij de eerste de beste bestand op
+pyautogui.click(65, 55, clicks=2)
+pyautogui.hotkey('delete')
+# hier haalt hij de text weg
+time.sleep(2)
+# hier typet inplaats van de naam een andere text
+pyautogui.write('Brave browser')
+pyautogui.hotkey('enter')
--- Take ss/sceenshotter.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(
-    r'/Applications/MAMP/htdocs/2e jaar/Module 2 Python/Take ss/deScreenshots/sceenshot.png')
